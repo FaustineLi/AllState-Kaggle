196be28ecd0c60967e4bea405a51da6fdf027e64
Faustine Li <faustineli12@gmail.com>
2016-11-20 21:36:04 -0500

    commit xgboost script.

--- xgboost_script.R
+++ xgboost_script.R
@@ -5,16 +5,18 @@ xtest = readRDS('xtest_lab.RDS')
 id = readRDS('testid.RDS')
 ytrain = readRDS('ytrain.RDS')
 
-dtrain = xgb.DMatrix(as.matrix(xtrain), label=data.matrix(log(ytrain + 200)))
+shift = 200
+dtrain = xgb.DMatrix(as.matrix(xtrain), label=data.matrix(log(ytrain + shift)))
 dtest =  xgb.DMatrix(as.matrix(xtest))
 
-# mae function
+# mae evaluation function
 xgb_eval = function (yhat, dtrain) {
     y = getinfo(dtrain, 'label')
     err = mean(abs(exp(y) - exp(yhat)))
     return (list(metric = 'error', value = err))
 }
 
+# log cosh objective function
 xgb_obj = function(yhat, dtrain) {
     y = getinfo(dtrain, 'label')
     grad = tanh(yhat - y)
@@ -26,37 +28,37 @@ xgb_obj = function(yhat, dtrain) {
 
 set.seed(0)
 xgb_params = list(
+    eta = 0.01,
+    colsample_bytree = 0.5,
+    subsample = 0.8,
+    max_depth = 12,
+    min_child_weight = 100,
+    gamma = 1,
+    alpha = 1,
+    lambda = 0,
+    base_score = 7,
     seed = 0,
-    colsample_bytree = 0.4,
-    subsample = 0.9,
-    eta = 0.001,
-    objective = xgb_obj,
-    max_depth = 6,
-    min_child_weight = 9,
-    gamma = 0,
-    alpha = 0,
-    lambda = 1,
-    base_score = 7)
+    objective = xgb_obj
+    )
 
 res = xgb.cv(data = dtrain,
              params = xgb_params,
-             nround = 5000,
+             nround = 100000,
              nfold = 5,
-             early_stopping_rounds = 20,
-             feval = xgb_mae,
+             early_stopping_rounds = 50,
+             feval = xgb_eval,
              maximize = FALSE)
 
 # ------------------------------------------------------------------
 set.seed(0)
-best = res$best_iteration
-model = xgboost(xtrain,
-                data.matrix(log(ytrain)),
+best = round(res$best_iteration)
+model = xgboost(dtrain,
                 params = xgb_params,
                 nrounds = best,
                 feval = xgb_eval,
                 maximize = FALSE)
 
-yhat = exp(predict(model, xtest))
+yhat = exp(predict(model, xtest)) - shift
 
 write.table(data.frame(id = id, loss = yhat),
             file = 'submit.csv', sep=',', row.names = FALSE)
